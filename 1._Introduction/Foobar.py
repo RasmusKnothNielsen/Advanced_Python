40a451d3d6676d1ae5b9ec5d4b021216c348e57a
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-02-08 11:43:59 +0100

    Changed minor typos in docstring

--- 1._Introduction/Foobar.py
+++ 1._Introduction/Foobar.py
@@ -20,7 +20,7 @@ def foo_bar(number, dictionary):
         Collection of numbers and corresponding words, that should be outputted each time the current
         number is divisible by the key in the dictionary.
 
-    :return: Void
+    :return: void
         The function prints the output to the console.
     """
     for i in range(number + 1):
