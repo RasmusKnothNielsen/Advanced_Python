6b1d9ecb2f345a455b10dd106ea1ce8e3ae6a335
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-02-16 18:36:36 +0100

    Added Docstrings and implemented configuration agnostic deletion method

--- 2._Python_Intro/BinaryTree.py
+++ 2._Python_Intro/BinaryTree.py
@@ -7,8 +7,8 @@
     Remember:
     - Document your code
     - DRY
-    - Seperate configuration from implementation
-    - Sigle Responsibility Principle
+    - Separate configuration from implementation
+    - Single Responsibility Principle
     - Test the code
     """
 
@@ -20,7 +20,7 @@ class Node:
 
     def __init__(self, *value):
         """
-        Initializing method that creates the binary tree and fills it with values
+        Initializing method that creates the binary tree and fills it with value(s)
 
         :param value: int or list
             an integer or a list of integers that are going to be added to the binary tree
@@ -70,6 +70,61 @@ class Node:
             else:
                 self.data = number
 
+    def delete_node(self, *data):
+        """
+        This method deletes the provided value(s) in the binary tree that it is called upon.
+
+        :param self: Node
+            The binary tree on which we want to delete a node from.
+
+        :param data: value(s)
+            A value or list of values that we want to delete from the binary tree
+
+        :return: Node
+            The function returns the binary tree without the deleted nodes.
+        """
+        for number in data:
+            # Base Case
+            if self is None:
+                return self
+
+            # If the key to be deleted is smaller than the root's
+            # key then it lies in  left subtree
+            if number < self.data:
+                self.left = self.left.delete_node(number)
+
+            # If the kye to be delete is greater than the root's key
+            # then it lies in right subtree
+            elif number > self.data:
+                self.right = self.right.delete_node(number)
+
+            # If key is same as root's key, then this is the node
+            # to be deleted
+            else:
+
+                # Node with only one child or no child
+                if self.left is None:
+                    temp = self.right
+                    self = None
+                    return temp
+
+                elif self.right is None:
+                    temp = self.left
+                    self = None
+                    return temp
+
+                # Node with two children: Get the in order successor
+                # (smallest in the right subtree)
+                temp = min_value_node(self.right)
+
+                # Copy the in order successor's content to this node
+                self.data = temp.data
+
+                # Delete the in order successor
+                self.right = self.right.delete_node(temp.data)
+
+        return self
+
     def print_tree(self):
         """
         Method to print the tree in chronological order
@@ -84,64 +139,6 @@ class Node:
         if self.right:
             result.append(self.right.print_tree())
         return result
-
-
-# Given a binary search tree and a key, this function
-# delete the key and returns the new root
-def delete_node(root, *data):
-    """
-    Given a binary tree and value(s), this function deletes the value(s) and returns the binary tree.
-
-    :param root: Node
-        The binary tree on which we want to delete a node from.
-
-    :param data: value(s)
-        A value or list of values that we want to delete from the binary tree
-
-    :return: Node
-        The function returns the binary tree without the deleted nodes.
-    """
-    for number in data:
-        # Base Case
-        if root is None:
-            return root
-
-        # If the key to be deleted is smaller than the root's
-        # key then it lies in  left subtree
-        if number < root.data:
-            root.left = delete_node(root.left, number)
-
-        # If the kye to be delete is greater than the root's key
-        # then it lies in right subtree
-        elif number > root.data:
-            root.right = delete_node(root.right, number)
-
-        # If key is same as root's key, then this is the node
-        # to be deleted
-        else:
-
-            # Node with only one child or no child
-            if root.left is None:
-                temp = root.right
-                root = None
-                return temp
-
-            elif root.right is None:
-                temp = root.left
-                root = None
-                return temp
-
-            # Node with two children: Get the in order successor
-            # (smallest in the right subtree)
-            temp = min_value_node(root.right)
-
-            # Copy the in order successor's content to this node
-            root.data = temp.data
-
-            # Delete the in order successor
-            root.right = delete_node(root.right, temp.data)
-
-    return root
 
 
 # Given a non-empty binary search tree, return the node
@@ -160,20 +157,36 @@ def min_value_node(node):
 if __name__ == '__main__':
     # Create a list of numbers to feed into the insert method
     list_of_numbers = [5, 2, 6, 9, 3, 1, 65, 23]
+
     # Initialize the tree with a list of numbers
+    print("Creating binary tree with a list of numbers")
     root = Node(*list_of_numbers)
-    # root.insert(*list_of_numbers)
     print(root.print_tree())
+    print()
+
+
     # Insert a single value
+    print("Inserting single values into the binary tree")
     root.insert(70)
     root.insert(10)
     print(root.print_tree())
+    print()
+
+    # Insert another list into the binary tree
+    print("Inserting another list into the binary tree")
+    new_list_of_numbers = [4, 14, 27, 51]
+    root.insert(*new_list_of_numbers)
+    print(root.print_tree())
+    print()
 
     # Deletes a single value
-    root = delete_node(root, 5)
+    print("Deletes a single value from the binary tree")
+    root.delete_node(5)
     print(root.print_tree())
+    print()
 
     # Deletes a list of values
+    print("Deletes a list of values from the binary tree")
     delete_numbers = [3, 9, 65]
-    root = delete_node(root, *delete_numbers)
+    root.delete_node(*delete_numbers)
     print(root.print_tree())
