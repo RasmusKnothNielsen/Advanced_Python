4fc15b92c2df956205fe42bc4e02dc74f49687a7
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-05-08 12:06:04 +0200

    Added Docstrings to Node class

--- Mandatory_Assignment_2/Pathfinder/Node.py
+++ Mandatory_Assignment_2/Pathfinder/Node.py
@@ -1,17 +1,36 @@
-class Node():
+class Node:
+    """ Each node represent a square on the path finding grid"""
 
-    def __init__(self, x=0, y=0, walkable=True):
-
+    def __init__(self, x=0, y=0, parent=None, walkable=True, h=0, g=0):
+        """ Initializer for the Node object
+        :param:
+            x, y = coordinates of the node
+            parent = Node from which we came, used for backtracking path.
+            h = distance from starting node
+            g = distance to ending node
+        """
         self.x = x
         self.y = y
 
+        self.parent = parent
+
+        self.h = h
+        self.g = g
+        self.f = g + h
+
         self.walkable = walkable
 
+    def __eq__(self, other):
+        """ Dunder method that checks if two points are the same """
+        return self.x == other.x and self.y == other.y
+
     def __lt__(self, other):
+        """ Dunder method that checks if the other nodes f score is larger than this one."""
 
         return self.f < other.f
 
     def cleanup(self):
+        """ Method to clean up the node when needed """
 
         self.h = 0.0
 
